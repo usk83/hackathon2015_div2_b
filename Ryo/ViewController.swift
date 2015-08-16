5588e4bdb4dda836dfbb027a3db526d3d79d5f92
Yusuke Takahashi <usk@sfc.wide.ad.jp>
2015-08-17 01:38:19 +0900

    背景画像を追加、画像サイズを調整し、夏な感じに

--- Ryo/ViewController.swift
+++ Ryo/ViewController.swift
@@ -27,15 +27,25 @@ class ViewController: UIViewController,AVAudioRecorderDelegate,AVAudioPlayerDele
 		var error:NSError?
 
 		super.viewDidLoad()
-		self.view.backgroundColor = UIColor.whiteColor()
+		// self.view.backgroundColor = UIColor.blackColor()
+		//self.view.backgroundColor = UIColor(patternImage: UIImage(named: "bg.jpg")!)
+        
+        let backgroundImage = UIImageView(image: UIImage(named: "bg.jpg")!)
+        backgroundImage.contentMode = UIViewContentMode.ScaleAspectFill
+        backgroundImage.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)
+        self.view.addSubview(backgroundImage)
 
 		let path = NSBundle.mainBundle().pathForResource("fan", ofType: "gif")!
 		let url = NSURL(fileURLWithPath: path)!
 		let animatedImage = FLAnimatedImage(animatedGIFData: NSData(contentsOfURL: url))
 
 		backgroundAnimationImage.animatedImage = animatedImage
-		backgroundAnimationImage.frame = view.bounds
-		view.insertSubview(backgroundAnimationImage, atIndex: 0)
+//		backgroundAnimationImage.frame = view.bounds
+        backgroundAnimationImage.frame = CGRectMake(0,0,130,272)
+        backgroundAnimationImage.center = CGPointMake(self.view.frame.width / 2, self.view.frame.height - 200)
+
+//		view.insertSubview(backgroundAnimationImage, atIndex: 0)
+        self.view.addSubview(backgroundAnimationImage)
 
 		//reverbの設定
 		reverb.loadFactoryPreset(.SmallRoom)
