409fd08fd7aec56dac7928e911d83a26942bef84
Yusuke Takahashi <usk@sfc.wide.ad.jp>
2015-08-16 22:24:18 +0900

    余計なコメントアウトの削除

--- Ryo/ViewController.swift
+++ Ryo/ViewController.swift
@@ -10,14 +10,6 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController,AVAudioRecorderDelegate,AVAudioPlayerDelegate {
-
-	// var _buttonFlag:Bool! = true
-	// var avRecorder:AVAudioRecorder?
-	// var avPlayer:AVAudioPlayer?
-
-	// let RecButton = UIButton(frame: CGRectMake(0,0,120,50))
-	// let PlayButton = UIButton(frame: CGRectMake(0,0,120,50))
-
 	var engine = AVAudioEngine()
 
 	let backgroundAnimationImage = FLAnimatedImageView()
@@ -45,26 +37,6 @@ class ViewController: UIViewController,AVAudioRecorderDelegate,AVAudioPlayerDele
 		backgroundAnimationImage.frame = view.bounds
 		view.insertSubview(backgroundAnimationImage, atIndex: 0)
 
-		// RecButton.backgroundColor = UIColor.redColor();
-		// RecButton.titleLabel?.font = UIFont.boldSystemFontOfSize(20)
-		// RecButton.setTitle("録音", forState: UIControlState.Normal)
-		// RecButton.layer.masksToBounds = true
-		// RecButton.layer.cornerRadius = 20.0
-		// RecButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
-		// RecButton.layer.position = CGPoint(x: self.view.bounds.width/2, y:self.view.bounds.height-150)
-		// RecButton.addTarget(self, action: "onTouch1:", forControlEvents: UIControlEvents.TouchUpInside)
-		// self.view.addSubview(RecButton)
-
-		// PlayButton.backgroundColor = UIColor.redColor();
-		// PlayButton.titleLabel?.font = UIFont.boldSystemFontOfSize(20)
-		// PlayButton.setTitle("再生", forState: UIControlState.Normal)
-		// PlayButton.layer.masksToBounds = true
-		// PlayButton.layer.cornerRadius = 20.0
-		// PlayButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
-		// PlayButton.layer.position = CGPoint(x: self.view.bounds.width/2, y:self.view.bounds.height-50)
-		// PlayButton.addTarget(self, action: "play", forControlEvents: UIControlEvents.TouchUpInside)
-		// self.view.addSubview(PlayButton)
-
 		//reverbの設定
 		reverb.loadFactoryPreset(.SmallRoom)
 		reverb.wetDryMix = 10
@@ -75,7 +47,6 @@ class ViewController: UIViewController,AVAudioRecorderDelegate,AVAudioPlayerDele
 
 		//eqの設定
 		eq.globalGain = 20
-
 
 		//engineにdelayとreverbを追加
 		engine.attachNode(reverb)
@@ -90,70 +61,6 @@ class ViewController: UIViewController,AVAudioRecorderDelegate,AVAudioPlayerDele
 		// engineを実行
 		engine.startAndReturnError(&error)
 	}
-
-	// func onTouch1(pSender:UIButton) {
-	// 	if(_buttonFlag == true) {
-	// 		record()
-	// 		RecButton.setTitle("停止", forState: UIControlState.Normal)
-	// 	}
-	// 	else {
-	// 		stop()
-	// 		RecButton.setTitle("録音", forState: UIControlState.Normal)
-	// 	}
-	// 	_buttonFlag = !_buttonFlag
-	// }
-
-	// func stop() {
-	// 	avRecorder?.stop()
-	// }
-
-	// func record() {
-	// 	let audioSession = AVAudioSession.sharedInstance()
-	// 	audioSession.requestRecordPermission { (granted) -> Void in
-	// 		if (granted) {
-	// 			if audioSession.inputAvailable {
-	// 				var error:NSError?
-	// 				audioSession.setCategory(AVAudioSessionCategoryRecord, withOptions: AVAudioSessionCategoryOptions.allZeros, error: &error)
-	// 				if ((error) != nil) {
-	// 					return
-	// 				}
-
-	// 				audioSession.setActive(true, error: &error)
-	// 				if ((error) != nil) {
-	// 					return
-	// 				}
-
-	// 				let filePaths = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)
-	// 				let documentDir: AnyObject = filePaths[0]
-	// 				let path = documentDir.stringByAppendingPathComponent("rec.caf")
-	// 				let recordingUrl = NSURL.fileURLWithPath(path)
-
-	// 				self.avRecorder = AVAudioRecorder(URL: recordingUrl, settings: nil, error: &error)
-	// 				self.avRecorder?.delegate = self
-	// 				if ((error) != nil) {
-	// 					return
-	// 				}
-	// 				self.avRecorder?.record()
-	// 			}
-	// 		}
-	// 	}
-	// }
-
-	// func play() {
-	// 	var error:NSError?
-	// 	let audioSession = AVAudioSession.sharedInstance()
-	// 	audioSession.setCategory(AVAudioSessionCategoryAmbient, withOptions: AVAudioSessionCategoryOptions.allZeros, error: &error)
-
-	// 	let filePaths = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)
-	// 	let documentDir: AnyObject = filePaths[0]
-	// 	let path = documentDir.stringByAppendingPathComponent("rec.caf")
-	// 	let recordingUrl = NSURL.fileURLWithPath(path)
-
-	// 	avPlayer = AVAudioPlayer(contentsOfURL: recordingUrl, error: &error)
-	// 	avPlayer?.delegate = self
-	// 	avPlayer?.volume = 1.0
-	// 	avPlayer?.play()
-	// }
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
