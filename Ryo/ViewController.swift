0e112660df3b40a134e3b0cd3b61c72d205606dd
butanosuke <booooo0630@gmail.com>
2015-08-16 15:33:33 +0900

    Initial Commit

---
+++ Ryo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ryo
+//
+//  Created by 駒田悠 on 2015/08/16.
+//  Copyright (c) 2015年 Butanosuke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
